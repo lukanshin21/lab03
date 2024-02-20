8e5adf8aa156296ce22118b58481ab3de9a52076
Leo <gordeev.lev.42@yandex.ru>
2024-02-20 20:28:25 +0500

    Сортировка пузырьком

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-def bubble_sort(n):
+def bubble_sort(n,k):
     arr = []
     for _ in range(n):
         num = int(input())
         arr.append(num)
-
-    for i in range(len(arr) - 1):
-        for j in range(0, len(arr) - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    if k == 1:
+        for i in range(len(arr) - 1):
+            for j in range(0, len(arr) - i - 1):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    elif k == 2:
+        for i in range(len(arr) - 1):
+            for j in range(0, len(arr) - i - 1):
+                if arr[j] < arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
     print("Отсортированный список: ")
     for num in arr:
         print(num)
 
-
+k = int(input("Введите 1, если по возрастанию, 2 - по убыванию: "))
 n = int(input("Введите число элементов: "))
-bubble_sort(n)
+bubble_sort(n,k)
